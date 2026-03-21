1331dd1b534cd0adb870819633de6de96373e55d
Juan Casas <imhouses19@gmail.com>
2026-03-21 16:12:13 -0600

    feat: auto-create database schema on startup in dev mode

--- src/main/kotlin/Databases.kt
+++ src/main/kotlin/Databases.kt
@@ -1,11 +1,26 @@
 package dev.jcasas
 
+import dev.jcasas.features.budgets.BudgetItems
+import dev.jcasas.features.budgets.Budgets
+import dev.jcasas.features.categories.Categories
+import dev.jcasas.features.transactions.Transactions
 import io.ktor.server.application.Application
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabases() {
     val url = environment.config.property("postgres.url").getString()
     val user = environment.config.property("postgres.user").getString()
     val password = environment.config.property("postgres.password").getString()
     Database.connect(url = url, user = user, password = password, driver = "org.postgresql.Driver")
+
+    val isDevelopment = environment.config
+        .propertyOrNull("ktor.development")?.getString()?.toBoolean() ?: false
+
+    if (isDevelopment) {
+        transaction {
+            SchemaUtils.create(Categories, Budgets, BudgetItems, Transactions)
+        }
+    }
 }
