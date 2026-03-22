51f32af2f4553e9dad422382d31fc834dfec1fad
Juan Casas <imhouses19@gmail.com>
2026-03-22 12:33:11 -0600

    WIP

--- kotlin-lint.sh
+++ kotlin-lint.sh
@@ -2,3 +2,8 @@
 set -euo pipefail
 
 ktlint -F "src/**/*.kt" "src/**/*.kts"
+
+if ! git diff --quiet -- 'src/**/*.kt' 'src/**/*.kts'; then
+  git add -u -- 'src/'
+  git commit -m "style: apply ktlint formatting"
+fi
